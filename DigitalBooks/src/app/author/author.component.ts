60b81e510af51b1e857f88d12b7235b6038764a1
unknown <cogjava3298@iiht.tech>
2022-10-30 22:58:20 +0530

    [Author] Added author component

--- DigitalBooks/src/app/author/author.component.ts
+++ DigitalBooks/src/app/author/author.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-author',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthorComponent implements OnInit {
 
-  constructor() { }
+  books!: Book[];
+
+  constructor(private bookService: BookService) { }
 
   ngOnInit(): void {
+
+    this.bookService.getAllBooks().subscribe(
+      data => this.books = data,
+      error => console.log("Error while fetching books for reader: " + error)
+    );
+
+  }
+
+  updateBook(book: Book){
+
+  }
+
+  blockBook(book: Book){
+
+  }
+
+  unblockBook(book: Book){
+
   }
 
 }
